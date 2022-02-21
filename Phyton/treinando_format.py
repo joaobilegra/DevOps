62cb41c82db858072bc6865835283e4d532a8f92
Joao Barbosa <joao.barbosa@ilegra.com>
2022-02-21 19:16:17 -0300

    Entrega phyton

---
+++ Phyton/treinando_format.py
@@ -0,0 +1,13 @@
+print("tentativa {} de {}".format(3,10))
+print("tentativa {1} de {0}".format(3,10))
+print("R${:.2f}".format(1.59))
+print("R${:.2f}".format(1.5))
+print("R${:.2f}".format(1258.5))
+
+print("R${:7.2f}".format(1.59))
+print("R${:7.2f}".format(1005.59)) 
+print("R${:7d}".format(4)) 
+print("R${:7d}".format(10000)) 
+print("R${:7d}".format(46)) 
+print("Data {:02d}/{:02d}".format(9,4)) 
+
