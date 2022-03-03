03e7ff7256137a14f3a7952a9edbd4a11a7178c8
Joao Barbosa <joao.barbosa@ilegra.com>
2022-03-03 17:49:41 -0300

    Entrega treino alura

--- Flask_Python/jogoteca/jogoteca.py
+++ Flask_Python/jogoteca/jogoteca.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from crypt import methods
+from urllib import request
+from flask import Flask, render_template , request
 app= Flask(__name__) 
 class Jogo:
     def __init__(self,nome, categoria, console):
@@ -15,9 +17,26 @@ jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
 lista = [jogo1, jogo2, jogo3]
 
 
-@app.route('/inicio')
-def ola():
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos',
                            jogos=lista)
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html',titulo= 'Novo jogo')
+    
+
+@app.route('/criar', methods=['POST',])   
+def criar():
+   nome= request.form['nome']
+   categoria= request.form['categoria']
+   console= request.form['console']
+   jogo= Jogo(nome, categoria, console)
+
+   lista.append(jogo)
+   return render_template('lista.html', titulo='jogos', jogos =lista)
+
+    
+
+app.run(debug=True)
