9dd04355482ed208c236e10dcbc0a5c6c943e089
Joao Barbosa <joao.barbosa@ilegra.com>
2022-03-04 18:18:25 -0300

    Entrega

--- Flask_Python/python_flask/Main.py
+++ Flask_Python/python_flask/Main.py
@@ -1,25 +1,46 @@
-from flask import Flask, render_template
-import Servico
+
+import Service
+from urllib import request
+from flask import Flask, redirect, render_template, request
 app= Flask(__name__)
+
+class Variable:
+    def __init__(self,name, contents):
+        self.name = name
+        self.contents = contents
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
  
+@app.route('/form')
+def form():
+    return render_template('form.html')
+  
+     
+@app.route('/create',methods=['POST',])   
+def creat():
+    name= request.form['name']
+    contents= request.form['contents']
+    variable1= Variable(name,contents)
+    return  redirect('env/'+variable1.name+'/'+variable1.contents)
+   
 
+ 
 @app.route('/config/env')
 def env_list():
-   return Servico.env_list()
+   return Service.env_list()
 
-@app.route("/env/<variable>/<content>")
-def create_env_var(variable, content):
- return Servico.create_var_linux(variable,content)
+@app.route("/env/<variable>/<contents>")
+def create_env_var(variable, contents):
+ return Service.create_var_linux(variable,contents)
 
 
 @app.route("/list/running_software")
 def list_running_software():
-    return Servico.list_running_software()
+    return Service.list_running_software()
 
 
-
-app.run(host='localhost',port=8080)
+app.run(host='0.0.0.0',port=8080)
 
