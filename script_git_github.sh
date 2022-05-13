4d872caf4d37130e0adaa06e81366f89af472291
joaobilegra <95234827+joaobilegra@users.noreply.github.com>
2022-05-13 10:35:16 -0300

    Update script_git_github.sh

--- script_git_github.sh
+++ script_git_github.sh
@@ -2,4 +2,4 @@ $1
 branch=$1
 git add .
 git commit -m "Entrega $branch"
-git push https://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@github.com/joaobarbosaRepo/DevOps.git
+git push https://segredo@github.com/joaobarbosaRepo/DevOps.git
